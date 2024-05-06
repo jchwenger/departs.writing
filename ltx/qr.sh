aa21c209880a716cc925a9748104d58280b2d495
jchwenger <34098722+jchwenger@users.noreply.github.com>
2024-05-06 11:46:37 +0100

    latex, qr | update

--- ltx/qr.sh
+++ ltx/qr.sh
@@ -2,11 +2,14 @@
 
 qrencode \
   --output images/qr.png \
-  --size 6 \
+  --size 20 \
   --level H \
+  --dpi 300 \
   --type PNG \
   "https://jeremiewenger.com/departs/"
 
 # annoyingly, the base png produced by qrencode isn't
 # rendered by xelatex, but the one below is
 convert images/qr.png images/qr.png
+
+cp images/qr.png ../card/departs.verso.png
